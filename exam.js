0ec242851f14aca0c19d2513ba17a19021c68caf
Zain-Sanwar <143992209+Zain-Sanwar@users.noreply.github.com>
2023-12-01 01:09:30 +0600

    first commit

---
+++ exam.js
@@ -0,0 +1,216 @@
+//Answer to Question 1:
+
+function addition (a, b){
+    return a + b;
+}
+
+console.log(addition(3, 2));
+
+
+
+//Answer to Question 2:
+
+function convert(minutes) {
+	return minutes * 60;
+}
+
+  console.log(convert(60));
+
+
+
+//Answer to Question 3:
+ 
+ function addition(number) {
+      return number + 1;
+
+  }
+  console.log(addition(1));
+
+
+
+
+//Answer to Question 4:
+
+function triArea(base, height) {
+
+    return (base * height) / 2;
+}
+
+console.log(triArea(8, 2));
+    
+   
+  
+
+//Answer to Question 5:
+
+function getFirstValue(array) {
+    
+    return array[0];
+    }
+
+    console.log(getFirstValue([2, 4, 6]));
+
+
+
+
+
+//Answer to Question 6:
+
+function calcAge(age) {
+    return age * 365;
+    }
+
+    console.log(calcAge(35));
+
+
+
+
+//Answer to Question 7:
+
+function circuitPower(voltage, current) {
+	return voltage * current;
+}
+
+console.log(circuitPower(3, 9));
+
+
+
+  
+//Answer to Question 8:
+
+function nextEdge(side1, side2){
+    return (side1 + side2) - 1;
+}
+
+console.log(nextEdge(5, 6));
+
+
+
+
+//Answer to Question 9:
+
+function remainder(a, b) {
+
+      return a % b;
+  }
+
+  console.log(remainder(5, 2));
+
+
+
+//Answer to Question 10:
+
+function giveMeSomething(a) {
+    
+      return "something" + "" + a;
+   
+  }
+
+  console.log(giveMeSomething("is better than nothing!"));
+
+
+
+//Answer to Question 11:
+
+function squared(number) {
+    return number * number;
+  }
+
+  console.log(squared(5));
+
+
+
+
+//Answer to Question 12:
+
+function lessThanOrEqualToZero(number) {
+
+    if (number > 0) {
+      return true;
+
+    } else {
+      return false;
+    }
+  }
+
+  console.log(lessThanOrEqualToZero(0));
+
+
+
+//Answer to Question 13:
+
+function sumPolygon(n) {
+
+    if (n > 2) {
+      return (n - 2) * 180;
+
+    } else {
+      return "Input is invalid.";
+    }
+  }
+
+console.log(sumPolygon(3));
+
+
+
+
+//Answer to Question 14:
+
+function nameString(name) {
+    var result = name + "Coder";
+    return result;
+  }
+
+  console.log(nameString("Matt"));
+
+
+
+//Answer to Question 15:
+
+function lessThan100(a, b) {
+
+	if(a + b < 100){
+        return true;
+
+    }else{
+        return false;
+    }
+}
+
+console.log(lessThan100(50, 50));
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+  
+
+
+
+  
+  
+
+
+
+
+
+  
+
+
+  
+
+
+
